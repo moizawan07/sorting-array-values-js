91b3158b3eaa3a3dbe31fd9f612c3df339b69183
moizawan07 <moizahmedawan321@gmail.com>
2024-09-30 09:46:20 -0700

    sorting-ass

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var numbers = [5, 7, 9, 8, 10, 3, 2];
 
-// expected result stored in an array
+document.getElementById("showw").innerHTML = numbers
 
+// expected result stored in an array
+// Acendind  Order
+
+var acendingOutput ;   
+function acending(){
 var check = [2,3,5,7,8,9,10]       
 
 var result = ["","","","","","","",]
@@ -15,4 +20,29 @@ for(var i = 0; i<numbers.length; i++){
     }  
    }
 }
-document.write(result);
+// document.write(result);
+acendingOutput = (result)
+document.getElementById("showa").innerHTML = acendingOutput
+}
+
+
+
+
+var dcendingOutput ;   
+function decending(){
+let check = [10,9,8,7,5,3,2]       
+
+let result = ["","","","","","","",]
+
+for(let i = 0; i<numbers.length; i++){ 
+      
+   for(let j =0; j<check.length; j++){
+    
+    if(numbers[i]==check[j]){
+      result.splice(j,1,numbers[i])
+    }  
+   }
+}
+dcendingOutput = (result)
+document.getElementById("showaa").innerHTML = dcendingOutput
+}
